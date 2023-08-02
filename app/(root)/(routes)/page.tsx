5d4a51032f97cf2e676bc4960547dff12f58609d
SorinC6 <kish.sorin@yahoo.com>
2023-08-02 10:49:52 +0300

    add categories section

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,6 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import { SearchInput } from "@/components/search-input";
 import prismadb from "../../../lib/prismadb";
+import { Categories } from "../../../components/categories";
 
 const RootPage = async () => {
   const categories = await prismadb.category.findMany();
@@ -9,6 +10,7 @@ const RootPage = async () => {
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 };
